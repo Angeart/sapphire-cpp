7617b14029d767a42944c9f3d9261229193febbd
ynakashima <ynakashima@aiming-inc.com>
2018-05-30 18:59:25 +0900

    fix few flagments

--- src/ast/variable.h
+++ src/ast/variable.h
@@ -8,5 +8,18 @@ struct variable_t : ::util::class_identity<variable_t,base_t> {
     std::string name;
     std::string type;
     std::string initializer;
+
+    variable_t() = default;
+
+    variable_t(const std::string& attribute_, const std::string& name_, const std::string& type_, const std::string& initializer_)
+    : attribute(attribute_), name(name_), type(type_), initializer(initializer_)
+    {}
+
+    bool operator== (const variable_t& rhs) const {
+        return this->attribute == rhs.attribute
+            && this->name == rhs.name
+            && this->type == rhs.type
+            && this->initializer == rhs.initializer;
+    }
 };
 }
