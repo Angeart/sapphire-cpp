c500beca96f2aed25add258c579be22c12628576
ynakashima <ynakashima@aiming-inc.com>
2018-06-08 18:54:42 +0900

    add skipper

--- src/grammar/skipper.hpp
+++ src/grammar/skipper.hpp
@@ -14,6 +14,16 @@ private:
     boost::spirit::qi::rule<iterator> root;
 };
 template<class iterator>
+class skipper_with_eol : public boost::spirit::qi::grammar<iterator> {
+public:
+    skipper_with_eol() : skipper_with_eol::base_type(root) {
+        namespace qi = boost::spirit::qi;
+        root = qi::omit[*(qi::blank | qi::eol)];
+    }
+private:
+    boost::spirit::qi::rule<iterator> root;
+};
+template<class iterator>
 class one_skipper : public boost::spirit::qi::grammar<iterator> {
 public:
     one_skipper() : one_skipper::base_type(root) {
