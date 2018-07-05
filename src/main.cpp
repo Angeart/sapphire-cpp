f0c27b37550baba3a0bd1bc5f1b5b7bac0f8bcaa
Angeart <shinoyuki.angeart@gmail.com>
2018-07-05 12:36:18 +0900

    change main

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
-#include "grammar/variable.hpp"
+#include "grammar/import.hpp"
 
 int main() {
-    sapphire::core::parser::variable<std::string::const_iterator> rule;
-    const std::string source = "lazy test_02 := hoge;";
+    sapphire::core::parser::import<std::string::const_iterator> rule;
+    const std::string source = "import TestModule1, TestModule2;";
     std::cout << "input : " << source << std::endl;
     std::cout << "====================================" << std::endl;
     auto it = source.begin();
     const auto& end = source.end();
-    sapphire::core::ast::variable_t result;
+    sapphire::core::ast::import_t result;
     if(!boost::spirit::qi::parse(it,end,rule,result) || it != end) {
         std::cout << "parse failed" << std::endl;
         std::cout << *it << std::endl;
     }
-    std::cout << "name : " << result.name << std::endl;
-    std::cout << "type : " << result.type << std::endl;
-    std::cout << "init : " << result.initializer << std::endl;
-    sapphire::core::ast::variable_t require = {::sapphire::core::types::variable_attribute::lazy, "test_02", "auto", ""};
-    std::cout << "same : " << (result == require) << std::endl;
+    std::string fstr = "", mstr = "";
+    for(auto f : result.functions) { fstr += f; }
+    for(auto m : result.modules) { mstr += m; }
+    std::cout << "functions : " << fstr << std::endl;
+    std::cout << "modules : " << mstr << std::endl;
 
 }
